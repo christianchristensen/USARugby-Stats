ffc134c33b0085e1511daaeac1dcbe2af21a13d2
Chris Christensen <christianchristensen@gmail.com>
2013-01-18 15:21:39 -0600

    Move around group management tasks and add bulk options.

--- app/groups_active.php
+++ app/groups_active.php
@@ -1,6 +1,4 @@
 <?php
-use Source\QueueHelper;
-
 include_once './include.php';
 use Source\APSource;
 
@@ -12,22 +10,47 @@ if (isset($_POST['submit'])) {
 
     echo '<div class="alert alert-success">Selected groups hidden.</div>';
 
-    $query = "UPDATE teams SET status='show'";
-    $result = mysql_query($query);
-    foreach ($_POST as $name => $value) {
-        $team_id = mysql_real_escape_string($value);
-        $query = "UPDATE teams SET status='hide' WHERE id=$team_id";
+    $submit = $request->get('submit');
+    if (strpos($submit, 'Hide All') === 0) {
+        $query = "UPDATE teams SET status='hide'";
         $result = mysql_query($query);
-        // $db->hideTeam($value);
+    }
+    elseif (strpos($submit, 'Show All') === 0) {
+        $query = "UPDATE teams SET status='show'";
+        $result = mysql_query($query);
+    }
+    elseif (strpos($submit, 'Show Teams Only') === 0) {
+        $query = "UPDATE teams SET status='hide'";
+        $result = mysql_query($query);
+        $query = "UPDATE teams SET status='show' WHERE type='team'";
+        $result = mysql_query($query);
+    }
+    else {
+        $query = "UPDATE teams SET status='show'";
+        $result = mysql_query($query);
+        foreach ($_POST as $name => $value) {
+            $team_id = mysql_real_escape_string($value);
+            $query = "UPDATE teams SET status='hide' WHERE id=$team_id";
+            $result = mysql_query($query);
+            // $db->hideTeam($value);
+        }
     }
 }
 
 $teams = $db->getAllTeams();
 ?>
+
+<h2>Group Management</h2>
+<label class="flabel" for="check_all">Manage teams to display in competitions by checking the groups you would like to display as hidden.</label>
+
+<form name="showhide" id="showhide" method="POST" action="">
+  <input class="btn" name="submit" type="submit" value="Show All" />
+  <input class="btn" name="submit" type="submit" value="Hide All" />
+  <input class="btn" name="submit" type="submit" value="Show Teams Only" />
+</form>
+<hr />
 <form name="teams_showhide" id="teams_showhide" method="POST" action="">
-  <h2>Group Management</h2>
-  <label class="flabel" for="check_all">Manage teams to display in competitions by checking the groups you would like to display as hidden.</label>
-  <input type="checkbox" name="check_all" id="active" value="Select All"/><span>Select All</span>  
+  <input type="checkbox" name="check_all" id="active" value="Select All"/><span>Select All</span>
   <input class="btn btn-warning btntop" name="submit" type="submit" value="Hide Groups" />
     <?php
     foreach ($teams as $uuid => $team) {
@@ -38,7 +61,8 @@ $teams = $db->getAllTeams();
         else {
             echo "<input class='grps-active' type='checkbox' name='team_{$team['uuid']}' value='{$team['id']}'>";
         }
-        echo "  {$team['name']} (<small>$uuid</small>)";
+        $type = ucfirst($team['type']);
+        echo "  {$team['name']} - {$team['description']} - {$type} (<small>$uuid</small>)";
         echo '</label>';
     }
     ?>
